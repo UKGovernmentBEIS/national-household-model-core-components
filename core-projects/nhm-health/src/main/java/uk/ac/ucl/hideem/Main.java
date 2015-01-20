8d0ca25000b03cc771873f7d43dc774a36773595
Tom Hinton <tom.hinton@cse.org.uk>
2015-01-20 17:45:53 +0000

    add some stuff to main.

--- core-projects/nhm-health/src/main/java/uk/ac/ucl/hideem/Main.java
+++ core-projects/nhm-health/src/main/java/uk/ac/ucl/hideem/Main.java
@@ -1,6 +1,10 @@
 package uk.ac.ucl.hideem;
 
 import java.util.Collections;
+import com.google.common.collect.*;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Hello world!
@@ -8,25 +12,54 @@ import java.util.Collections;
  */
 public class Main
 {
-    public static void main( String[] args )
+    /**
+     * This is in no way best practice. Error checking etc etc.
+     */
+    static Person readPerson(final Map<String, String> stuff) {
+        return new Person(
+            Integer.parseInt(stuff.get("age")),
+            Person.Sex.valueOf(stuff.get("sex")),
+            Boolean.valueOf(stuff.get("smokes")));
+    }
+    
+    public static void main( String[] args ) throws IOException
     {
         final HealthModule module = new HealthModule();
 
-        //TODO read useful things from here
-        while (true) {
-            System.out.println(module.effectOf(
-                                   0, 0,
-                                   0, 0,
-                                   BuiltForm.Detached,
-                                   100,
-                                   0,
-                                   1990,
-                                   true,
-                                   true,
-                                   1,
-                                   Collections.<Person>emptyList(),
-                                   10));
-            break;
+        // load the people into here; keyed on house ID
+        // format expected to be:
+        // code, age, sex, smoker
+        final ListMultimap<String, Person> people = ArrayListMultimap.create();
+
+        System.out.println("Reading people from " + args[0]);
+
+        for (final Map<String, String> row : CSV.mapReader(Paths.get(args[0]))) {
+            final Person p = readPerson(row);
+            people.put(row.get("code"), p);
+        }
+
+        // now process the houses
+        System.out.println("Reading house data from " + args[1]);
+
+        for (final Map<String, String> row : CSV.mapReader(Paths.get(args[1]))) {
+            // blah blah
+            final HealthOutcome outcome = module.effectOf(
+                // get fields from row here
+                Double.parseDouble(row.get("e1")),
+                Double.parseDouble(row.get("e2")),
+                Double.parseDouble(row.get("p1")),
+                Double.parseDouble(row.get("p1")),
+                BuiltForm.valueOf(row.get("form")),
+                Double.parseDouble(row.get("area")),
+                Integer.parseInt(row.get("level")),
+                Integer.parseInt(row.get("buildyear")),
+                Boolean.valueOf(row.get("extract")),
+                Boolean.valueOf(row.get("trickle")),
+                Integer.parseInt(row.get("fans")),
+                people.get(row.get("code")),
+                Integer.parseInt(row.get("horizon")));
+            System.out.println(row.get("code"));
+            System.out.println(outcome);
         }
     }
 }
