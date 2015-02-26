27ccdd832eb20bfbab2e11af2a5b0266d3b82d83
phy6phs <philip.hugh.symonds@cern.ch>
2015-02-26 12:02:37 +0000

    Rewrite of health code to match Scribe (needs testing)

---
+++ core-projects/nhm-health/src/main/java/uk/ac/ucl/hideem/Constants.java
@@ -0,0 +1,49 @@
+package uk.ac.ucl.hideem;
+
+public class Constants {
+    //Health coefficients
+	public static final double REL_RISK_SIT_CV 	= 	0.913043;
+	public static final double REL_RISK_ETS_CA 		= 	1.25;
+    public static final double REL_RISK_ETS_MI 		= 	1.3;
+    public static final double REL_RISK_PM_CP 		= 	1.082;
+    public static final double REL_RISK_PM_LC		= 	1.059;
+    public static final double REL_RISK_RADON_LC 	= 	1.16;
+    public static final double REL_RISK_SIT_CMD 	= 	0.902952;
+    public static final double REL_RISK_MOULD_ASTHMA1 	= 	1.83;
+    public static final double REL_RISK_MOULD_ASTHMA2 	= 	1.53;
+    public static final double REL_RISK_MOULD_ASTHMA3 	= 	1.53;
+    public static final double WEIGHT_CMD 			= 	0.88;
+    public static final double WEIGHT_ASTHMA1 		= 	0.97;
+    public static final double WEIGHT_ASTHMA2 		= 	0.85;
+    public static final double WEIGHT_ASTHMA3 		= 	0.669;
+    public static final double PREV_ASTHMA1  		= 	0.093;
+    public static final double PREV_ASTHMA2	 		=	0.016;
+    public static final double PREV_ASTHMA3	 		= 	0.001;
+    public static final double PREV_CMD	 			= 	0.15;
+    public static final double INC_WINCV			= 	1;
+    public static final double INC_PM_CP			= 	10;
+    public static final double INC_PM_LC 			= 	10;
+    public static final double INC_ETS_CA 			= 	1;
+    public static final double INC_ETS_MI 			= 	1;
+    public static final double INC_RADON_LC	 		= 	100;
+    public static final double INC_WINCMD	 		= 	1;
+    public static final double INC_MOULD_ASTHMA1 	= 	100;
+    public static final double INC_MOULD_ASTHMA2 	=   100;
+    public static final double INC_MOULD_ASTHMA3 	= 	100;
+    
+    //Time functions
+    //public final double[][] timeFunction;// = new double[4][3];
+    public static final double[] TIME_FUNCTION (final Disease.Type disease) {
+        double[] tFunc = new double[]{1, 1, 1};;
+    	
+    	if (disease == Disease.Type.cerebrovascular || disease == Disease.Type.myocardialinfarction) {
+    		tFunc = new double[]{5, 2, 0.4};
+        } else if (disease == Disease.Type.cardiopulmonary) {
+        	tFunc = new double[]{5, 2, 0.3};
+        } else if (disease == Disease.Type.lungcancer) {
+        	tFunc = new double[]{15, 2, 0.2};
+        } 
+    	
+        return tFunc;
+    }
+}
