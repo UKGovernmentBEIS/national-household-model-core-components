97389563a61abf358705b29966a5fa5ed86699d3
Tom Hinton <tom.hinton@cse.org.uk>
2015-10-21 16:45:15 +0100

    Add constructor so it compiles

--- core-projects/nhm-health/src/main/java/uk/ac/ucl/hideem/CumulativeHealthOutcome.java
+++ core-projects/nhm-health/src/main/java/uk/ac/ucl/hideem/CumulativeHealthOutcome.java
@@ -1,9 +1,13 @@
 package uk.ac.ucl.hideem;
 
 /**
- * A health outcome class which cares only about cumulative effects
+ * A health outcome class which cares only about accumulated effects.
  */
 public class CumulativeHealthOutcome extends HealthOutcome {
+    public CumulativeHealthOutcome(final int horizon) {
+
+    }
+
     public void addEffects(final Disease.Type disease, final int year, final Person whom,
                            final double mortality, final double morbidity, final double cost) {
 
