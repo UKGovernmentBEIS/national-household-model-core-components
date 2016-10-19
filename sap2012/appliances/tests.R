03ac44cc235670aa18d90571c559c3456aacb4b3
Glenn Searby <glenn.searby@gmail.com>
2016-10-19 11:33:03 +0100

    SAP2012 appliances and energy-constants test (the R test not yet filled out)

---
+++ sap2012/appliances/tests.R
@@ -0,0 +1,9 @@
+## Tests 2 things:
+
+result <- load.probe("result")
+
+## TODO: both SAP rows should be the same (context.energy-constants has no effect)
+
+## TODO: both BREDEM rows are different by the expected amount (context.energy-constants has an effect)
+
+## TODO: the SAP and BREDEM rows with the default values are different by the expected amount when using their default values
