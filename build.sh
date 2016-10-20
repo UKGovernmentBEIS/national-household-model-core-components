dc801d08fb0c6c8eba18a5f2defe27c8828e4556
Tom Hinton <tom.hinton@cse.org.uk>
2016-10-20 14:16:00 +0100

    Optional build docs; maven settings; sign keys
    build.sh now has arguments:
    --no-documentation (skip docs)
    --include-api (don't skip api bundle)
    --stupid-gradle (use gradle rather than ./gradlew)
    
    build.sh prints its output in green and errors in red
             errors should be printed out at the end as well
    
    maven settings are now included in the repo; build.sh supplies them
    to maven.
    
    the documentation plugin is set to be optional in the IDE
    so it can be skipped.

--- build.sh
+++ build.sh
@@ -1,37 +1,111 @@
-#!/bin/bash
-echo "Building and installing core projects to local maven repo"
-cd core-projects/
-./gradlew clean install -x test
-cd ..
+#!/usr/bin/env bash
+GRADLE="./gradlew"
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --no-documentation)
+            SKIP_DOCUMENTATION=1
+            ;;
+        --stupid-gradle)
+            GRADLE="gradle"
+            ;;
+        --include-api)
+            BUILD_API=1
+            ;;
+        --help)
+            echo "usage: ./build.sh [--no-documentation] [--stupid-gradle] [--include-api]"
+            exit 0
+            ;;
+    esac
+    shift # use up this argument
+done
 
-#echo "Stating package-drone p2 server"
-cd org.eclipse.packagedrone.server-0.12.2
-java -jar plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar &
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+ERRS=""
+
+red () {
+    printf "${RED}WARN${NC} $*\n"
+    ERRS="$ERRS\n$*"
+}
+
+green () {
+    printf "${GREEN}INFO${NC} $*\n"
+}
+
+# use maven global settings from this folder
+SETTINGS="$PWD/maven-settings.xml"
+maven () {
+    mvn -gs "$SETTINGS" "$@"
+    RES=$?
+    if [ $RES -ne 0 ]; then
+        red "ERROR: mvn $@ => $RES (in $PWD)"
+    fi
+}
+
+# my machine (tom) has some problem with gradlew binaries.
+gradle () {
+    $GRADLE "$@"
+    RES=$?
+    if [ $RES -ne 0 ]; then
+        red "ERROR: gradle $@ => $RES (in $PWD)"
+    fi
+}
+
+green "Building and installing core projects to local maven repo"
+pushd core-projects/
+gradle clean install -x test
+popd
+
+./start_p2.sh &
 SERVER=$!
-cd ..
+green "Started P2 Server [$SERVER]"
 
-echo "Building and publising nhm-api-bundle"
-cd core-projects/nhm-bundle-api
-./gradlew clean publish
-cd ../..
+if [ ! -z $BUILD_API ]; then
+    green "Building and publising nhm-api-bundle"
+    pushd core-projects/nhm-bundle-api
+    gradle clean publish
+    popd
+else
+    green "skipping API"
+fi
 
-echo "Building and publishing nhm-impl-bundle"
-cd core-projects/nhm-impl-bundle
-./gradlew clean publish
-cd ../..
+green "Building and publishing nhm-impl-bundle"
+pushd core-projects/nhm-impl-bundle
+gradle clean publish
+popd
 
 #TODO Not sure why the ide doesn't pick this up from p2
-echo "Building and publishing documentation bundle"
-cd nhm-documentation
-mvn install
-cd eclipse
-mvn deploy
-cd ../..
 
-echo "Building the ide"
-cd nhm-ide
-mvn clean package
+if [ -z "$SKIP_DOCUMENTATION" ]; then
+    green "Building and publishing documentation bundle"
+    pushd nhm-documentation
+    maven install
+    cd eclipse
+    maven deploy
+    popd
+else
+    green "skipping documentation"
+fi
 
-echo "Stopping p2 server...I'm lying not sure how todo this yet sorry ;-)"
-#TODO: How to stop the drone server? Something with grep on process list to get process id to kill?
-kill $!
+pushd nhm-ide
+
+green "Getting signing keys submodule"
+git submodule init
+git submodule update
+
+green "Building IDE"
+maven clean package
+
+popd
+
+green "Stopping p2 server [$SERVER]..."
+kill $SERVER
+
+if [ ! -z $ERRS ]; then
+    red "$ERRS"
+else
+    green "IDE built into nhm-ide/nhm-ide/cse.nhm.ide.build/target/products/"
+fi
