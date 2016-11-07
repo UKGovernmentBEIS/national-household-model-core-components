07c9f6a9e068cdff6ee794133294739aac496362
Glenn Searby <glenn.searby@gmail.com>
2016-11-07 12:22:15 +0000

    Fixed wrong number of solar gains overshading factors (solar gains source test now passes again).

--- core-projects/energycalculator.impl/src/main/java/uk/org/cse/nhm/energycalculator/constants/EnergyCalculatorConstants.java
+++ core-projects/energycalculator.impl/src/main/java/uk/org/cse/nhm/energycalculator/constants/EnergyCalculatorConstants.java
@@ -21,7 +21,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	*/
 	@ConstantDescription("This converts from M3/hr of air changes to W/degree temperature difference.")
 	VENTILATION_HEAT_LOSS_COEFFICIENT(0.33),
-	
+
 	@ConstantDescription("This is the number of extra air changes per hour caused by storeys of the house above the ground floor")
 	STACK_EFFECT_AIR_CHANGE_PARAMETER(0.1),
 
@@ -33,15 +33,15 @@ public enum EnergyCalculatorConstants implements IConstant {
 	UNIT: ach/h
 	SAP: (13)
 	BREDEM: Table 19
-	DEPS: 
-	GET: 
+	DEPS:
+	GET:
 	SET: context.energy-calculator-constants
 	ID: draught-lobby-constant
 	CODSIEB
 	*/
 	@ConstantDescription("This is the number of air changes per hour caused by not having a draught lobby (double front doors)")
-	DRAUGHT_LOBBY_AIR_CHANGE_PARAMETER(0.05), 
-	
+	DRAUGHT_LOBBY_AIR_CHANGE_PARAMETER(0.05),
+
 	/*
 	BEISDOC
 	NAME: Wind factor divisor
@@ -55,8 +55,8 @@ public enum EnergyCalculatorConstants implements IConstant {
 	CODSIEB
 	*/
 	@ConstantDescription("The wind exposure factor is divided by this number")
-	WIND_FACTOR_DIVISOR(4.0), 
-	
+	WIND_FACTOR_DIVISOR(4.0),
+
 	/*
 	BEISDOC
 	NAME: Unresponsive heating system temperature reduction
@@ -70,12 +70,12 @@ public enum EnergyCalculatorConstants implements IConstant {
 	CODSIEB
 	*/
 	@ConstantDescription("This is the amount below demand temperature a fully unresponsive heating system is assumed to provide.")
-	UNRESPONSIVE_HEATING_SYSTEM_DELTA_T(2.0), 
-	
+	UNRESPONSIVE_HEATING_SYSTEM_DELTA_T(2.0),
+
 	/*
 	BEISDOC
 	NAME: Metabolic gains per person
-	DESCRIPTION: The heat gain per person due to human metabolism. 
+	DESCRIPTION: The heat gain per person due to human metabolism.
 	TYPE: value
 	UNIT: W/person
 	SAP: (66), Table 5
@@ -90,7 +90,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	/*
 	BEISDOC
 	NAME: Reduced Metabolic gains per person
-	DESCRIPTION: The heat gain per person due to human metabolism when using reduced internal gains. 
+	DESCRIPTION: The heat gain per person due to human metabolism when using reduced internal gains.
 	TYPE: value
 	UNIT: W/person
 	SAP: Table 5
@@ -100,7 +100,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	*/
 	@ConstantDescription("The gains per occupant, in watts due to metabolism")
 	REDUCED_METABOLIC_GAINS_PER_PERSON(50.0),
-	
+
 	/*
 	BEISDOC
 	NAME: Evaporation loss per person
@@ -115,7 +115,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	*/
 	@ConstantDescription("The gains per occupant due to evaporation, in watts")
 	EVAPORATION_GAINS_PER_PERSON(40.0),
-	
+
 	/*
 	BEISDOC
 	NAME: Gains Overshading factor
@@ -123,7 +123,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	TYPE: 5 values (one for each overshading type)
 	UNIT: Dimensionless
 	SAP: Table 6d (winter solar access factor column)
-	BREDEM: Table 23 
+	BREDEM: Table 23
 	SET: context.energy-constants
 	DEPS: overshading
 	NOTES: Only the middle overshading factor is ever used.
@@ -131,12 +131,12 @@ public enum EnergyCalculatorConstants implements IConstant {
 	CODSIEB
 	*/
 	@ConstantDescription("The multipliers for OvershadingType which affect the degree of solar gains.")
-	SOLAR_GAINS_OVERSHADING(new double[] {1.0, 1.0, 0.77, 0.54, 0.3}), 
-	
+	SOLAR_GAINS_OVERSHADING(new double[] {1.0, 0.77, 0.54, 0.3}),
+
 	/*
 	BEISDOC
 	NAME: Solar gains reflection
-	DESCRIPTION: A constant multiplier due to reflection which reduces solar gains. 
+	DESCRIPTION: A constant multiplier due to reflection which reduces solar gains.
 	TYPE: value
 	UNIT: Dimensionless
 	SAP: (74-82)
@@ -147,7 +147,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	*/
 	@ConstantDescription("The reflection factor in solar gains (how much solar gain is lost because of reflectivity)")
 	SOLAR_GAINS_REFLECTION_FACTOR(0.9),
-	
+
 	/*
 	BEISDOC
 	NAME: Reference Heat Loss Parameter
@@ -177,7 +177,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	*/
 	@ConstantDescription("The heat loss parameter multiplier used in calculating the time constant")
 	TIME_CONSTANT_HEAT_LOSS_PARAMETER_MULTIPLIER(3.6),
-	
+
 	/*
 	BEISDOC
 	NAME: Minimum Cooling Time
@@ -192,7 +192,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	*/
 	@ConstantDescription("The minimum cooling time, for average temperature calculation")
 	MINIMUM_COOLING_TIME(4.0),
-	
+
 	/*
 	BEISDOC
 	NAME: Cooling time time constant multiplier
@@ -207,7 +207,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	*/
 	@ConstantDescription("Cooling time time constant multiplier, for average temperature calculation")
 	COOLING_TIME_TIME_CONSTANT_MULTIPLIER(0.25),
-	
+
 	/*
 	BEISDOC
 	NAME: Utilisation Factor Time Constant Divisor
@@ -222,7 +222,7 @@ public enum EnergyCalculatorConstants implements IConstant {
 	*/
 	@ConstantDescription("Divisor of time constant for utilisation factor calculation")
 	UTILISATION_FACTOR_TIME_CONSTANT_DIVISOR(15.0),
-	
+
 	/*
 	BEISDOC
 	NAME: Threshold degree days value
@@ -232,13 +232,13 @@ public enum EnergyCalculatorConstants implements IConstant {
 	BREDEM: 8F, 8G
 	SET: context.energy-constants
 	NOTES: This is the 1/5 which the degree days are set to if the upper and lower threshold temperatures are equal.
-	NOTES: It is also the multiple of 5 inside the exponential, when they are not equal. 
+	NOTES: It is also the multiple of 5 inside the exponential, when they are not equal.
 	ID: threshold-degree-days-value
 	CODSIEB
 	*/
 	@ConstantDescription("Threshold degree days value, used for determining heating on factor")
-	THRESHOLD_DEGREE_DAYS_VALUE(5.0), 
-	
+	THRESHOLD_DEGREE_DAYS_VALUE(5.0),
+
 	/*
 	BEISDOC
 	NAME: Gains Utilisation Factor Threshold Difference
@@ -248,12 +248,12 @@ public enum EnergyCalculatorConstants implements IConstant {
 	BREDEM: 8D
 	SET: context.energy-constants
 	CONVERSION: This number is increased by 0.5 compared to BREDEM, because we skip out the +0.5 and convert the -0.5 into -1 later on in the calculation.
-	ID: gains-utilisation-factor-threshold-difference 
+	ID: gains-utilisation-factor-threshold-difference
 	CODSIEB
 	*/
 	@ConstantDescription("The amount below the demand temperature the threshold temperature is for gains utilisation factor calculation")
 	GAINS_UTILISATION_FACTOR_THRESHOLD_DIFFERENCE(4.5),
-	
+
 	/*
 	BEISDOC
 	NAME: Base window infiltration
@@ -261,15 +261,15 @@ public enum EnergyCalculatorConstants implements IConstant {
 	TYPE: value
 	UNIT: ach/h
 	SAP: (15)
-	DEPS: 
-	GET: 
+	DEPS:
+	GET:
 	SET: context.energy-constants
 	ID: window-infiltration-constant
 	CODSIEB
 	*/
 	@ConstantDescription("The air change rate due to doors and windows for a house where none of the doors and windows are draught stripped")
-	WINDOW_INFILTRATION(0.25), 
-	
+	WINDOW_INFILTRATION(0.25),
+
 	/*
 	BEISDOC
 	NAME: Draught stripped factor
@@ -277,15 +277,15 @@ public enum EnergyCalculatorConstants implements IConstant {
 	TYPE: value
 	UNIT: ach/h
 	SAP: (15)
-	DEPS: 
-	GET: 
+	DEPS:
+	GET:
 	SET: context.energy-constants
 	ID: draught-stripped-factor-constant
 	CODSIEB
 	*/
 	@ConstantDescription("The reduction in air change rate due to doors and windows for a house where all of the doors and windows are draught stripped")
-	DRAUGHT_STRIPPED_FACTOR(0.2), 
-	
+	DRAUGHT_STRIPPED_FACTOR(0.2),
+
 	/*
 	BEISDOC
 	NAME: Sheltered sides exposure factor
@@ -294,15 +294,15 @@ public enum EnergyCalculatorConstants implements IConstant {
 	UNIT: Dimensionless
 	SAP: (20)
 	BREDEM: Table 22
-	DEPS: 
-	GET: 
+	DEPS:
+	GET:
 	SET: context.energy-constants
 	ID: sheltered-sides-exposure-factor
 	CODSIEB
 	*/
 	@ConstantDescription("Proportional reduction in air changes for to each side of the house which is sheltered")
 	SHELTERED_SIDES_EXPOSURE_FACTOR(0.075),
-	
+
 	/*
 	BEISDOC
 	NAME: Site Exposure Factor Lookup
@@ -316,28 +316,28 @@ public enum EnergyCalculatorConstants implements IConstant {
 	*/
 	// The related categories are in the SiteExposureType enum.
 	SITE_EXPOSURE_FACTOR(new double[]{1.10, 1.05, 1.00, 0.95, 0.90}),
-	
+
 	@ConstantDescription("Ventilation rate for open flue")
-	OPEN_FLUE_VENTILATION_RATE(20.0), 
-	
+	OPEN_FLUE_VENTILATION_RATE(20.0),
+
 	@ConstantDescription("Ventilation rate for chimney.")
 	CHIMNEY_VENTILATION_RATE(40.0)
 	;
-	
+
 	private double[] multipleValues;
 	private double defaultValue;
-	
-	private EnergyCalculatorConstants(double defaultValue) {
+
+	private EnergyCalculatorConstants(final double defaultValue) {
 		this.defaultValue = defaultValue;
 	}
-	
-	private EnergyCalculatorConstants(double[] values) {
+
+	private EnergyCalculatorConstants(final double[] values) {
 		defaultValue = 0;
 		multipleValues = values;
 	}
-	
+
 	@Override
-	public <T> T getValue(Class<T> clazz) {
+	public <T> T getValue(final Class<T> clazz) {
 		if (Double.class.isAssignableFrom(clazz)) {
 			return clazz.cast((Double) defaultValue);
 		} else if (double[].class.isAssignableFrom(clazz)) {
