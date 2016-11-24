91302b4db98117053c6084dcfbab08e4f39263d5
Glenn Searby <glenn.searby@gmail.com>
2016-11-24 15:57:28 +0000

    Made thermal-bridging test work.
    It currently fails, but that's probably because of houses without
    external walls, which is a good reason to fail...

--- system-tests/sap2012/thermal-bridging-factor/tests.R
+++ system-tests/sap2012/thermal-bridging-factor/tests.R
@@ -1,19 +1,37 @@
 energy <- load.probe("energy")
 
-## TODO: check this actually works
+## Prove that modifying the thermal bridging levels for a house:
+## 1. Improves energy use for space heating under BREDEM
+## 2. Has no effect under SAP
 
-energy <- energy[energy$bredem..Before != 0] # Exclude houses with no energy use (no occupants)
+energy <- energy[energy$bredem..Before != 0,] # Exclude houses with no energy use (no occupants)
 
-bredem_did_not_fall <- energy[energy$bredem..After >= energy$bredem..Before]
+bredem_rose <- energy[energy$bredem..After > energy$bredem..Before,]
+bredem_stayed_constant <- energy[energy$bredem..After == energy$bredem..Before,]
 
 fail.test.if(
-    any(bredem_did_not_fall),
-    paste("Space heating energy use should fall under BREDEM 2012 when the thermal bridging factor is reduced.", bredem_did_not_fall)
+    nrow(bredem_rose) > 0,
+    paste(
+        "Space heating energy use should not rise under BREDEM 2012 when the thermal bridging factor is reduced.",
+        paste(bredem_rose$dwelling.id, collapse = ", ")
+    )
 )
 
-sap_changes <_ energy[energy$sap..After != energy$sap..Before]
+## Currently failing, probably because of dwellings without external walls.
+fail.test.if(
+    nrow(bredem_stayed_constant) > 0,
+    paste(
+        "Space heating energy use should fall under BREDEM 2012 when the thermal bridging factor is reduced.",
+        paste(bredem_stayed_constant$dwelling.id, collapse = ", ")
+    )
+)
+
+sap_changes <- energy[energy$sap..After != energy$sap..Before,]
 
 fail.test.if(
-    any(sap_changes),
-    paste("Space heating energy use should not change under SAP 2012 when the thermal bridging factor is changed, since it is hardcoded to 0.15 in SAP 2012 mode.", sap_changes)
+    nrow(sap_changes) > 0,
+    paste(
+        "Space heating energy use should not change under SAP 2012 when the thermal bridging factor is changed, since it is hardcoded to 0.15 in SAP 2012 mode.",
+        paste(sap_changes$dwelling.id, collapse = ", ")
+    )
 )
