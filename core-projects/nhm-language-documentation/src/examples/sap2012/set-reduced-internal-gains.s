e3afd59873fdfebebbe63bf593f36769c2cddc76
Glenn Searby <glenn.searby@gmail.com>
2016-11-28 12:17:15 +0000

    Example scenarios relating to the SAP 2012 changes for the manual.

---
+++ core-projects/nhm-language-documentation/src/examples/sap2012/set-reduced-internal-gains.s
@@ -0,0 +1,6 @@
+(example
+ caption: "Using Reduced Internal Gains"
+ description: "Shows how to put a dwelling onto reduced internal gains (appropriate for modern dwellings).
+Once a dwelling is using reduced internal gains, they cannot be disabled."
+
+ (action.reduced-internal-gains))
