7f138cafe4fcd881dac17dc034fb1dcf1eb2e249
Glenn Searby <glenn.searby@gmail.com>
2016-11-28 12:32:38 +0000

    Obsoleted house.buildyear, because it does not meet the NHM naming conventions. Added house.build-year.

--- core-projects/nhm-language-definition/src/main/java/uk/org/cse/nhm/language/definition/function/house/XBuildYear2.java
+++ core-projects/nhm-language-definition/src/main/java/uk/org/cse/nhm/language/definition/function/house/XBuildYear2.java
@@ -1,18 +1,15 @@
 package uk.org.cse.nhm.language.definition.function.house;
 
+import com.larkery.jasb.bind.Bind;
+
 import uk.org.cse.nhm.language.definition.Doc;
-import uk.org.cse.nhm.language.definition.Obsolete;
 import uk.org.cse.nhm.language.definition.SeeAlso;
 import uk.org.cse.nhm.language.definition.function.bool.house.XBuildYearIs;
 import uk.org.cse.nhm.language.definition.function.num.XHouseNumber;
 
-import com.larkery.jasb.bind.Bind;
-
-
-@Bind("house.buildyear")
+@Bind("house.build-year")
 @Doc("The year of construction of the house.")
 @SeeAlso(XBuildYearIs.class)
-@Obsolete(inFavourOf = {XBuildYear2.class}, reason = "Did not follow the NHM's naming convention.", version = "6.5.0")
-public class XBuildYear extends XHouseNumber {
+public class XBuildYear2 extends XHouseNumber {
 
 }
