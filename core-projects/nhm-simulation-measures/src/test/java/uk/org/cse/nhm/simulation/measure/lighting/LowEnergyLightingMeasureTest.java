84fc0c59033b74a07188260f8b105e5d27c42ea3
Glenn Searby <glenn.searby@gmail.com>
2017-06-01 16:33:23 +0100

    measure.low-energy-lighting wasn't actually installing any low energy lighting, but was removing the normal lighting.
    In most dwellings this actually gave the desired effect by accident, but
    it resulted in 0 energy consumption for lighting in dwellings which
    didn't have any existing low energy lighting.
    
    Fixed this and added a test for it.

--- core-projects/nhm-simulation-measures/src/test/java/uk/org/cse/nhm/simulation/measure/lighting/LowEnergyLightingMeasureTest.java
+++ core-projects/nhm-simulation-measures/src/test/java/uk/org/cse/nhm/simulation/measure/lighting/LowEnergyLightingMeasureTest.java
@@ -2,11 +2,9 @@ package uk.org.cse.nhm.simulation.measure.lighting;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import uk.org.cse.commons.names.Name;
 import uk.org.cse.nhm.hom.emf.technologies.ILight;
 import uk.org.cse.nhm.hom.emf.technologies.ITechnologiesFactory;
@@ -31,44 +29,62 @@ public class LowEnergyLightingMeasureTest {
 		scope = mock(ISettableComponentsScope.class);
 		techDim = mock(IDimension.class);
 		structureDim = mock(IDimension.class);
-		
+
 		tech = ITechnologiesFactory.eINSTANCE.createTechnologyModel();
 		when(scope.get(techDim)).thenReturn(tech);
-		
+
 		structure = mock(StructureModel.class);
 		when(structure.getFloorArea()).thenReturn(1d);
 		when(scope.get(structureDim)).thenReturn(structure);
-		
+
 		lets = mock(ILets.class);
 	}
-	
+
 	@Test
 	public void suitability() {
 		final LowEnergyLightingMeasure measure = build(0.5, 1.0);
-		
+
 		Assert.assertFalse("Unsuitable if no lights.", measure.isSuitable(scope, lets));
-		
+
 		tech.getLights().add(light(ILight.CFL_EFFICIENCY));
 		Assert.assertFalse("Unsuitable if not enough inefficient lights.", measure.isSuitable(scope, lets));
-		
+
 		tech.getLights().add(light(ILight.INCANDESCENT_EFFICIENCY));
 		tech.getLights().add(light(ILight.INCANDESCENT_EFFICIENCY));
 		Assert.assertTrue("Unsuitable if enough inefficient lights.", measure.isSuitable(scope, lets));
 	}
-	
+
 	private ILight light(final double eff) {
 		final ILight l = ITechnologiesFactory.eINSTANCE.createLight();
 		l.setProportion(1.0);
 		l.setEfficiency(eff);
 		return l;
 	}
-	
+
 	private LowEnergyLightingMeasure build(final double threshold, final double proportion) {
-		
-		
-		return new LowEnergyLightingMeasure(threshold, proportion, 
-				ConstantComponentsFunction.<Number>of(Name.of("capex"), 10), 
-				techDim, 
+
+
+		return new LowEnergyLightingMeasure(threshold, proportion,
+				ConstantComponentsFunction.<Number>of(Name.of("capex"), 10),
+				techDim,
 				structureDim);
+	}
+
+	@Test
+	public void installLowEnergyLights() {
+		final ILight badLights = ITechnologiesFactory.eINSTANCE.createLight();
+		badLights.setEfficiency(ILight.INCANDESCENT_EFFICIENCY);
+		badLights.setProportion(1);
+
+		tech.getLights().add(badLights);
+
+		final LowEnergyLightingMeasure measure = build(0, 1);
+		measure.createModifier(scope, lets, 1d).modify(tech);
+
+		Assert.assertEquals("The rubbish lights should have been removed, and new lights installed.", 1, tech.getLights().size());
+
+		final ILight goodLights = tech.getLights().get(0);
+		Assert.assertEquals("The remaining lights should have better efficiency.", ILight.CFL_EFFICIENCY, goodLights.getEfficiency(), 0.0);
+		Assert.assertTrue("The remaining lights should have a proportion greater than 0.", goodLights.getProportion() > 0);
 	}
 }
