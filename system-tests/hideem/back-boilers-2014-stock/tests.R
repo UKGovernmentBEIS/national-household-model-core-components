da51edec7f764d6000b961a89a407ebefa40c031
trickyBytes <richard.tiffin@akoolla.com>
2017-10-24 15:30:12 +0100

    Tests for osticket 160

---
+++ system-tests/hideem/back-boilers-2014-stock/tests.R
@@ -0,0 +1,11 @@
+result <- load.probe("hideem-report")
+
+results <- data.table(result)
+back.boiler.replaced <- results[boiler.type..Before. == "BackBoiler" & suitable == "true",]
+permeability.changed <- back.boiler.replaced[permeability..Before. != permeability..After., .N]
+
+fail.test.if(permeability.changed > 0, "Permability does not change when back-boiler is replaced")
+
+write.csv(result,file.path("~/software-projects/nhm-cse/system-tests/t.csv"))
+
+
