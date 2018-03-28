0ce65c50d15d75856309c2f62216a9842a6884eb
Tom Hinton <tom.hinton@cse.org.uk>
2018-03-28 17:05:06 +0100

    Repair tests for local build

--- system-tests/hideem/fan-adjustment/tests.R
+++ system-tests/hideem/fan-adjustment/tests.R
@@ -2,9 +2,9 @@
 result <- load.probe("hideem-report")
 
 fail.test.if(
-    !result$no.of.fans..Before. < result$no.of.fans..After.,
+    any(result$no.of.fans..Before. >= result$no.of.fans..After.),
     "Number of fans should have increased by 1")
 
 fail.test.if(
-    !result$permeability..Before. < result$permeability..After.,
+    any(result$permeability..Before. > result$permeability..After.),
     "Permeability should increase.")
