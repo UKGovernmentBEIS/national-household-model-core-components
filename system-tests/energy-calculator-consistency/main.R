119ace098ffb1b5e4c7ab72dea116a08f704779c
Tom Hinton <tom.hinton@cse.org.uk>
2018-07-12 14:18:45 +0100

    Improve test to print more diagnostic values

--- system-tests/energy-calculator-consistency/main.R
+++ system-tests/energy-calculator-consistency/main.R
@@ -4,22 +4,29 @@ expectation <- read.table("stored-values.tab", sep="\t", header = TRUE)
 
 comparison <- load.probe("energy") %>%
     select(survey.code = survey.code..Before.,
+           weight = weight..Before.,
            bredem2012 = bredem2012..Before.,
            sap2012 = sap2012..Before.) %>%
     distinct %>%
     {
-        write.table(., "new-values.tab", sep="\t", row.names = FALSE, col.names = TRUE)
+        write.table(select(., -weight),
+                    "new-values.tab", sep="\t", row.names = FALSE, col.names = TRUE)
         .
     } %>%
     full_join(expectation, by="survey.code") %>%
     mutate(delta.bredem = bredem2012.x - bredem2012.y,
-           delta.sap = sap2012.x - sap2012.y) %>%
-    select(survey.code, delta.bredem, delta.sap)
+           delta.sap = sap2012.x - sap2012.y,
+           bredem.pc = 100*(delta.bredem / bredem2012.x),
+           sap.pc = 100*(delta.sap / sap2012.x)) %>%
+    select(survey.code, weight, delta.bredem, delta.sap, bredem.pc, sap.pc)
 
-bad.rows <- filter(comparison, delta.bredem > 0.01 | delta.sap > 0.01)
+bad.rows <- filter(comparison, (abs(delta.bredem) > 0.01) | (abs(delta.sap) > 0.01))
 
 if (nrow(bad.rows)) {
     print(bad.rows)
+    print(summary(bad.rows$bredem.pc))
+    print(summary(bad.rows$sap.pc))
+    write.table(bad.rows, "errors.tab", sep="\t", row.names=FALSE, col.names=TRUE)
 }
 
 fail.test.if(with(comparison, abs(delta.bredem) > 0.01),
